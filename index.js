723bf8324f09905f5af0a6c12faacdf172d2b077
lollomac <lorenzo.meccoli@gmail.com>
2017-03-18 20:11:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,64 @@ app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
-/*
+//HEROKU WEBHOOKS FB
+var bodyParser = require('body-parser');
+var xhub = require('express-x-hub');
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+
+app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('It works!');
+});
+
+app.get(['/facebook', '/instagram'], function(req, res) {
+  if (
+    req.param('hub.mode') == 'subscribe' &&
+    req.param('hub.verify_token') == 'token'
+  ) {
+    res.send(req.param('hub.challenge'));
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/facebook', function(req, res) {
+  console.log('Facebook request body:');
+
+  if (req.isXHub) {
+    console.log('request header X-Hub-Signature found, validating');
+    if (req.isXHubValid()) {
+      console.log('request header X-Hub-Signature validated');
+      res.send('Verified!\n');
+    }
+  }
+  else {
+    console.log('Warning - request header X-Hub-Signature not present or invalid');
+    res.send('Failed to verify!\n');
+    // recommend sending 401 status in production for non-validated signatures
+    // res.sendStatus(401);
+  }
+  console.log(req.body);
+
+  // Process the Facebook updates here
+  res.sendStatus(200);
+});
+
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
+  res.sendStatus(200);
+});
+
+app.listen();
+
+/* PARSE WEBHOOKS FB
 app.get(['/facebook', '/instagram'], function(req, res) {
   if (
     req.param('hub.mode') == 'subscribe' &&
