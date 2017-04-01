97a562c8057fe3b6146f2e33406d86d2e92072ca
lollomac <lorenzo.meccoli@gmail.com>
2017-04-01 18:42:51 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
   res.send('It works!');
 });
 
-/*
+
 app.get(['/facebook', '/instagram'], function(req, res) {
   if (
     req.param('hub.mode') == 'subscribe' &&
@@ -38,7 +38,7 @@ app.post('/instagram', function(req, res) {
   // Process the Instagram updates here
   res.send(200);
 });
-
+/*
 app.listen();
 */
 
