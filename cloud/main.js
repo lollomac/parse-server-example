e8ae1b13720fbe30009a0389fd3a346b9751d8d3
lollomac <lorenzo.meccoli@gmail.com>
2017-04-01 14:26:36 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
   res.send('It works!');
 });
 
-/*
+
 app.get(['/facebook', '/instagram'], function(req, res) {
   if (
     req.param('hub.mode') == 'subscribe' &&
@@ -40,7 +40,7 @@ app.post('/instagram', function(req, res) {
 });
 
 app.listen();
-*/
+
 
 
 Parse.Cloud.define('hello', function(req, res) {
