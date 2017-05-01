19c9d1891e3b6ab090a6d32393a9bde26181435f
lollomac <lorenzo.meccoli@gmail.com>
2017-05-02 00:25:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ app.post('/facebook', function(req, res) {
   console.log('request header X-Hub-Signature validated');
   // Process the Facebook updates here
   received_updates.unshift(req.body);
+  test();
   res.sendStatus(200);
 });
 
