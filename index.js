ba31bd1b2ca679db429b3909f7898c6c416667db
lollomac <lorenzo.meccoli@gmail.com>
2017-05-02 20:49:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var xhub = require('express-x-hub');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
-//var cloudCode = require('./cloud/main.js');
+var cloudCode = require('./cloud/main.js');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
